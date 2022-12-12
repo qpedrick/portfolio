844d9b8f8ffebb000d4c8dff9d2dd9b196196912
Quinton Pedrick <77907943+qpedrick@users.noreply.github.com>
2022-12-12 05:43:53 -0600

    added new branch for a rework

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import Header from './Components/Header';
 import React from 'react';
+import Header from './Components/Header';
 import About from './Components/About';
 import Projects from './Components/Projects';
 import Footer from './Components/Footer';
